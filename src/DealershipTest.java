e091be7ff00f65032a903f5130178306b632a931
Joselito <jaguardado123@gmail.com>
2024-10-22 11:42:56 -0500

    Added more test cases

--- src/DealershipTest.java
+++ src/DealershipTest.java
@@ -2,21 +2,39 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DealershipTest {
+
+	@Test
+	public void testing_car_make() {
+        Car car = new Car("Toyota", "Camry", 2000);
+		assertEquals("Toyota", car.make);
+	}
+
+	@Test
+	public void testing_car_model() {
+        Car car = new Car("Ford", "Mustang", 2011);
+		assertEquals("Mustang", car.model);
+	}
+
+	@Test
+	public void testing_car_year() {
+        Car car = new Car("Honda", "Civic", 2005);
+		assertEquals(2005, car.year);
+	}
 	
 	@Test
-	public void testing_car1() {
+	public void testing_car_2000() {
         Car car = new Car("Toyota", "Camry", 2000);
 		assertEquals(false, car.is_new());
 	}
 
 	@Test
-	public void testing_car2() {
+	public void testing_car_2024() {
         Car car = new Car("Ford", "Mustang", 2024);
 		assertEquals(true, car.is_new());
 	}
 
 	@Test
-	public void testing_car3() {
+	public void testing_car_2025() {
         Car car = new Car("Chevy", "Silverado", 2025);
 		assertEquals(true, car.is_new());
 	}
