cc33f875073a7ead7cec87b3598bb87a2f8baae1
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-30 12:25:47 -0500

    Update and rename Dealership.java to Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Paste the README code snippet here.
+
+    }
+}
+
+// Write your Car and Buyer class in the Car.java and Buyer.java files.
